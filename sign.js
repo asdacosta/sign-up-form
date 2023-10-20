d5cf5b2eb1ab66626bd67fd73acfe36888d3c35e
asdacosta <asdacosta18@gmail.com>
2023-10-20 13:16:30 +0000

    Apply style to valid name input

--- sign.js
+++ sign.js
@@ -8,3 +8,12 @@ const mailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 const telRegex = /^(\+\d{1,2}\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/;
 const pwdRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
+names.forEach(name => {
+    name.addEventListener('change', () => {
+        if (nameRegex.test(name.value)) {
+            name.style.backgroundColor = 'rgb(205, 223, 178)';
+        } else {
+            name.style.backgroundColor = 'rgb(221, 146, 146)';
+        }
+    })
+})
